9d6cc9a123f9168dde8e116a4a190f7024f1925f
Rupavathi913 <rupavathimullapati@gmail.com>
2026-01-20 16:45:28 +0530

    Add files via upload

---
+++ Signup.js
@@ -0,0 +1,7 @@
+function signupUser() {
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
+let confirm = document.getElementById("confirm").value;
+let phone = document.getElementById("phone").value;
+}
